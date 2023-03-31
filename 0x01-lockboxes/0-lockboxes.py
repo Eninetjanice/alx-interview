fb9082b1d95d20f6288766e55ed7929ce188bd5b
Janeth Eni <enijaneth@gmail.com>
2023-03-31 18:38:39 +0100

    Clean up

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 """
-This implements a simple algorithm to determine if all boxes in a list of lists can be opened using keys contained in other boxes. The function returns True if all boxes can be opened and False if otherwise.
+This implements a simple algorithm to determine if all boxes in a list of
+lists can be opened using keys contained in other boxes. The function returns
+True if all boxes can be opened and False if otherwise.
 """
+
+
 def canUnlockAll(boxes):
     """ Function to determine if all boxes can be opened """
     # Initialize the list of unlocked boxes with box 0
@@ -16,12 +20,11 @@ def canUnlockAll(boxes):
 
         # Iterate over each key in the box
         for key in box:
-             # If key unlocks an unlocked box && isn't the current box,
-             # append to the list of unlocked boxes
+            # If key unlocks unlocked box & != current box, add 2 [unlocked]
             if key < len(boxes) and key not in unlocked and key != box_id:
                 unlocked.append(key)
 
-    # If num of unlocked boxes == total num of boxes, return True, else return false
+    # If num of unlocked boxes == total num of boxes, return True, else false
     if len(unlocked) == len(boxes):
         return True
     return False
