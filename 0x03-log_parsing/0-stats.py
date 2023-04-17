e144dfc8359e9d3f05334613ba8c8c33b5679915
Janeth Eni <enijaneth@gmail.com>
2023-04-17 05:50:29 +0100

    Update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 ''' Define a function to print the metrics '''
 
-import sys
+from sys import stdin
+
+
+def parse_input(line):
+    """ Extract the file size and status code from each line of input."""
+    fields = line.split()
+    if len(fields) < 7:
+        return None
+    try:
+        size = int(fields[-1])
+        status_code = int(fields[-2])
+        return (size, status_code)
+    except ValueError:
+        return None
 
 
 def print_stats(total_size, status_codes):
@@ -12,38 +25,32 @@ def print_stats(total_size, status_codes):
             print("{}: {}".format(key, status_codes[key]))
 
 
-def parse_input(line):
-    """ Extract the file size and status code from each line of input."""
-    try:
-        fields = line.split(" ")
-        size = int(fields[-1])
-        code = int(fields[-2])
-        return (size, code)
-    except Exception:
-        return None
-
-
 def main():
     """
     Initializes variables used for tracking total file size & num of lines.
     Reads standard input line by line, updates the var accordingly, &
     prints stats every 10 lines &/or when keyboard interruption occurs
     """
+    line_num = 0
     total_size = 0
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0,
-                    500: 0}
-    count = 0
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+
     try:
-        for line in sys.stdin:
+        for line in stdin:
+            line_num += 1
             parsed = parse_input(line)
             if parsed:
-                size, code = parsed
+                size, status_code = parsed
                 total_size += size
-                if code in status_codes:
-                    status_codes[code] += 1
-                count += 1
-            if count % 10 == 0:
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+
+            if line_num % 10 == 0:
                 print_stats(total_size, status_codes)
+
+        print_stats(total_size, status_codes)
+
     except KeyboardInterrupt:
         print_stats(total_size, status_codes)
         # sys.exit(0)
